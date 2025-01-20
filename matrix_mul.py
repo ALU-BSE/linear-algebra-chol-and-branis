597645a790ba9792e8dc6e4a9ee94e78809c6b5b
Chol Daniel Deng Dau <144565388+Dau2004@users.noreply.github.com>
2025-01-20 21:03:32 +0200

    Update matrix_mul.py

--- matrix_mul.py
+++ matrix_mul.py
@@ -1,18 +1,11 @@
-import pandas as pd
-import numpy as np
-
-# Load data
-data = pd.read_csv('path/to/data')
-
-# Example arrays
+#Maltiplication of Matrices
 Prices = [[300, 500],
           [1000, 120.85]]
 
 Array2 = [200, 100]
 
-# Calculate the result
-# Ans = []
-# (300*200 + 500*100) as an example calculation
+
+#Looping through
 
 for i in range(len(Prices)):
   for j in range(len(Prices[0])):
