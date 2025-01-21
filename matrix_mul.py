b712bf1596ca779e175f4fe446a8c65c742f2b9f
Chol Daniel Deng Dau <144565388+Dau2004@users.noreply.github.com>
2025-01-21 18:40:24 +0200

    Update matrix_mul.py

--- matrix_mul.py
+++ matrix_mul.py
@@ -2,14 +2,16 @@
 Prices = [[300, 500],
           [1000, 120.85]]
 
-Array2 = [200, 100]
+Total = [200, 100]
 
 
 #Looping through
 
 for i in range(len(Prices)):
   for j in range(len(Prices[0])):
-    res1 = ((Prices[0,0])*(Total[0])) + ((Prices[0,1])*(Total[1]))
-    res2 = ((Prices[1,0])*(Total[0])) + ((Prices[1,1])*(Total[1]))
+    res1 = ((Prices[0][0])*(Total[0])) + ((Prices[0][1])*(Total[1]))
+    res2 = ((Prices[1][0])*(Total[0])) + ((Prices[1][1])*(Total[1]))
 
 print(res1, res2)
+
+
